79e744c24488699ded3a7d2fc5982f5b4b8473c2
Shoyu Vanilla <modulo641@gmail.com>
2023-12-07 02:20:42 +0900

    Add pwsh aliases

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -11,6 +11,23 @@ Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 # FNM - Fast Node Managr
 fnm env --use-on-cd | Out-String | Invoke-Expression
 
+# Aliases
+Set-Alias -Name df -Value duf
+
+Set-Alias -Name ls -Value eza
+Function Fll {eza -l --group-directories-first --sort=ext}
+Set-Alias -Name ll -Value Fll
+Function Fld {eza -lD}
+Set-Alias -Name ld -Value Fld # List Directories
+Function Flf {eza -lf --sort=ext}
+Set-Alias -Name lf -Value Flf # List Files
+Function Fla {eza -al --group-directories-first --sort=ext}
+Set-Alias -Name la -Value Fla # List All
+Function Flt {eza -al --sort=modified}
+Set-Alias -Name lt -Value Flt # List by Time
+
+# zoxide for jumping directories
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
+# starship shell prompt
 Invoke-Expression (&starship init powershell)
